2ac5bacfe37eee8872d1ec5a542acb7ebbeaaa43
Amadou Sarjo Barry <152334531+TSApp5@users.noreply.github.com>
2024-01-14 16:22:57 +0100

    Update app.component.ts

--- frontend 2/src/app/app.component.ts
+++ frontend 2/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router'; // Import RouterModule instead of RouterOutlet
 import { LoaderService } from './loader.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterModule], // Use RouterModule instead of RouterOutlet
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // Use 'styles' instead of 'styleUrl'
 })
 export class AppComponent {
-  constructor(public loaderService: LoaderService){}
+  constructor(public loaderService: LoaderService) {}
   title = 'frontend';
 }
